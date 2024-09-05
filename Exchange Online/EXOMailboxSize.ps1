53dc08441f915f65d6267be3f1aab2b30afeafcd
OfirGavish <81085873+OfirGavish@users.noreply.github.com>
2024-09-05 19:23:36 +0300

    Added email settings variables, and threshold variables

--- Exchange Online/EXOMailboxSize.ps1
+++ Exchange Online/EXOMailboxSize.ps1
@@ -3,8 +3,8 @@
 This script is located at https://github.com/OfirGavish/PSscripts/edit/main/Exchange%20Online/EXOMailboxSize.ps1.
 
 .DESCRIPTION
-This script is used for detection of Exchange Online Mailboxes with over 70 GB of Used space.
-If the mailbox is over 70GB, the script will check if the mailbox is over 90GB and if it is, it will enable the Archive and AutoExpandingArchive for that mailbox.
+This script is used for detection of Exchange Online Mailboxes with over a specific ammount of Used space.
+If the mailbox is over first threshold, the script will check if the mailbox is over the second threshold and if it is, it will enable the Archive and AutoExpandingArchive for that mailbox.
 
 .PARAMETER None
 There are no parameters for this script.
@@ -12,8 +12,9 @@ There are no parameters for this script.
 .EXAMPLE
 Example usage of the script:
     run in azure automation runbook, make sure to assign the correct permissions to the managed identity.
+    set the threshold values, set the email settings and set the "Organization" in the Connect command at line 37
     email is sent using Azure Email Communication Service, feel free to change the email settings.
-    You can export the information to a CSV file and send it as an attachment, uncomment line 65 and add the attachment flag to the Send-MailMessage command.
+    You can export the information to a CSV file and send it as an attachment, uncomment line 80 and add the attachment flag to the Send-MailMessage command.
 
 .NOTES
 Author: Ofir Gavish
@@ -21,37 +22,51 @@ Special Thanks to Ram Apter @ https://github.com/Rmap91
 Date: 09/05/2024
 #>
 
-#Connect to Exchange Online
+# Email settings
+$from = "mailboxalerts@domain.com"
+$to = "EXOTeam@domain.com"
+$cred = Get-AutomationPSCredential -Name 'CredentialName'
+$emailserver = "smtp.azurecomm.net"
+$emailport = 587
+
+# Set the threshold sizes as desired
+$threshold1 = 70GB
+$threshold2 = 90GB
+
+# Connect to Exchange Online
 Connect-ExchangeOnline -ManagedIdentity -Organization domain.onmicrosoft.com
-#Get all mailboxes statistics
+
+# Get all mailboxes statistics
 $mailbox = Get-EXOMailbox -ResultSize unlimited | Get-EXOMailboxStatistics
-#initialize alert array
+
+# Initialize alert array
 $alert = @()
-#Loop through all mailboxes, check if the mailbox is over 70GB, if it is, check if it is over 90GB and enable Archive and AutoExpandingArchive
+
+# Loop through all mailboxes, check if the mailbox is over the first threshold, if it is, check if it is over the second threshold and enable Archive and AutoExpandingArchive
 try {
-    foreach ($ml in $mailbox) {
-        if ($ml.TotalItemSize.Value -gt 70GB) {
-            $AE = Get-ExoMailbox $ml -PropertySets Archive
-            if ($ml.TotalItemSize.Value -gt 90GB -and !$AE.AutoExpandingArchiveEnabled) 
+    foreach ($mb in $mailbox) {
+        if ($mb.TotalItemSize.Value -gt $threshold1) {
+            $AE = Get-ExoMailbox $mb -PropertySets Archive
+            if ($mb.TotalItemSize.Value -gt $threshold2 -and !$AE.AutoExpandingArchiveEnabled) 
             {
                 try 
                 {
                     if ($AE.ArchiveStatus -eq "None")
                     {
-                        Enable-Mailbox $ml -Archive
+                        Enable-Mailbox $mb -Archive
                     }
-                    Enable-Mailbox $ml -AutoExpandingArchive
-                    $AE = Get-ExoMailbox $ml -PropertySets Archive
+                    Enable-Mailbox $mb -AutoExpandingArchive
+                    $AE = Get-ExoMailbox $mb -PropertySets Archive
                 }
                 catch {
                     Throw "Failed to Enable Archive or AutoExpandingArchive: $_"
                 }
             }
-            #Add the mailbox to the alert array with the relevant information
+            # Add the mailbox to the alert array with the relevant information
             $alert += [PSCustomObject]@{
-                DisplayName = $ml.DisplayName
-                TotalDeletedItemSize = $ml.TotalDeletedItemSize
-                TotalItemSize = $ml.TotalItemSize
+                DisplayName = $mb.DisplayName
+                TotalDeletedItemSize = $mb.TotalDeletedItemSize
+                TotalItemSize = $mb.TotalItemSize
                 AutoExpandingArchiveEnabled = $AE.AutoExpandingArchiveEnabled
             }
         }
@@ -61,8 +76,8 @@ catch {
     Throw "Failed to run the foreach loop: $_"
 }
 
-#Export the alert array to a CSV file
-#$alert | Export-Csv -Path "$env:Temp\test23324.csv" -NoTypeInformation
+# Export the alert array to a CSV file
+#$alert | Export-Csv -Path "$env:Temp\EXOMailboxAlert.csv" -NoTypeInformation
 
 # Create HTML table from $alert
 $htmlTable = $alert | ConvertTo-Html -Property DisplayName, TotalDeletedItemSize, TotalItemSize, AutoExpandingArchiveEnabled -As Table
@@ -93,17 +108,16 @@ $htmlTable
 </html>
 "@
 
-#Send email with the alert information
-$from = "mailboxalerts@domain.com"
-$to = "EXOTeam@domain.com"
-$cred = Get-AutomationPSCredential -Name 'CredentialName'
+
+# Send email with the alert information
 try
 {
-    Send-MailMessage -To $to -From $from -Credential $cred -Subject "Mailboxes over 70GB Alerts" -Body $htmlbody -BodyAsHtml -SmtpServer "smtp.azurecomm.net" -Port 587 -UseSsl
+    Send-MailMessage -To $to -From $from -Credential $cred -Subject "Mailboxes over $threshold1 Alerts" -Body $htmlbody -BodyAsHtml -SmtpServer $emailserver -Port $emailport -UseSsl
 }
 catch
 {
     throw $Error.Exception.Message
 }
-#Disconnect from Exchange Online
+
+# Disconnect from Exchange Online
 Disconnect-ExchangeOnline
